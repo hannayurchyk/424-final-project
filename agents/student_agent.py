613a6c2285dcbcc5f124618291d390a1eaca7f34
Hanna <annayurchyk@hotmail.com>
2022-12-04 12:42:59 -0500

    agent tweaks that are good against random agent

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -10,17 +10,16 @@ from random import randint
 import time
 
 TIME_DELTA = 0.05
-COMPUTATION_TIME = 2
-FIRST_COMPUTATION_TIME = 2
-NUM_ROLLOUTS = 10
-ROLLOUT_DECAY = 0.1
-NODES_TO_EXPAND = 2
-UCT_EXPLORATION_RATE = 0.7
+COMPUTATION_TIME = 1.999
+FIRST_COMPUTATION_TIME = 29.990
+NUM_ROLLOUTS = 20
+ROLLOUT_DECAY = 0.3
+NODES_TO_EXPAND = 3
+UCT_EXPLORATION_RATE = 0.5
 
-def flatten(l):
-    return [item for sublist in l for subsublist in sublist for item in subsublist]
-
+# Define the heuristic according to which we want to rank the states of the game
 def heuristic(state):
+    # state: Node
     def get_walls(state, adv=True):
         walls = []
         if adv:
@@ -29,13 +28,20 @@ def heuristic(state):
             walls = state.chess_board[state.my_pos[0]][state.my_pos[1]]
         return list(walls).count(True)
     
+    # Compute the ranking features 
     adv_walls = get_walls(state)
     my_walls = get_walls(state, adv=False)
-    dist_to_adv = abs(state.my_pos[0]-state.adv_pos[0]) + abs(state.my_pos[1]-state.adv_pos[1])
+    dist_to_adv = abs(state.my_pos[0]-state.adv_pos[0]) + abs(state.my_pos[1]-state.adv_pos[1]) # Compute the Manhattan distance to the opponent 
     
-    return adv_walls - math.exp(my_walls) - dist_to_adv
+    # Return the weights of the heuristic features 
+    return adv_walls - (0 if my_walls < 3 else -math.inf) + dist_to_adv
 
 
+# Helper method to compare chess_board used to override equals method in Node class
+def flatten(l):
+    return [item for sublist in l for subsublist in sublist for item in subsublist]
+
+# Code used by the world class to check if an endgame is reached 
 def endgame(p0_pos, p1_pos, chess_board):
     '''
     Returns true if the game has ended, and false otherwise. Is used by MCTS to verify the endgame during rollouts.
@@ -89,18 +95,18 @@ def endgame(p0_pos, p1_pos, chess_board):
     
 class Node:    
     def __init__(self, parent, chess_board, my_pos, my_dir, adv_pos, agent_turn=True):
-        self.parent = parent
+        self.parent = parent 
         self.chess_board = chess_board
         self.wins = 0 # Number of wins in rollouts (includes children nodes)
         self.visits = 0 # Number of rollouts (includes children)
-        self.children = []
-        self.possible_children = []
-        self.my_pos = my_pos
-        self.my_dir = my_dir
+        self.children = [] # List of tuples containing (Node, Node.heuristic())
+        self.possible_children = [] # List of tuples containing (Node, Node.heuristic())
+        self.my_pos = my_pos # Tuple (row, col)
+        self.my_dir = my_dir # Int from 0 to 3 for directions up, right, down, left 
         self.adv_pos = adv_pos
-        self.agent_turn = agent_turn
-        self.expanded = False
-        self.bad = False
+        self.agent_turn = agent_turn # Boolean indicating which agent's turn it is (for the first run we will have True for 2 depths)
+        self.expanded = False # Used to see if the step was expanded in our search according to our guess, or if it is contained in all the possible moves
+        self.bad = False # Indicating that a leaf node leads to a loss TODO verify if correctly implemented
         
     def __eq__(self, other):
         if not (self.my_pos == other.my_pos and self.adv_pos == other.adv_pos):
@@ -111,9 +117,11 @@ class Node:
     def __contains__(self, item):
         return any(item, lambda x : self.__eq__(item))
     
+    # Heuristic is overriden in StudentAgent class
     def heuristic(self):
         return 1
             
+    # Computes the Upper Confidence Tree of the Node        
     def uct_value(self, current_state):       
         # If the node was not visited, we will prioritize the exploration of the  node 
         # by setting its value to infinity
@@ -126,21 +134,26 @@ class Node:
         # Otherwise return its confidence bound value
         return self.wins / self.visits + UCT_EXPLORATION_RATE * math.sqrt(math.log(current_state)/self.visits)
     
+    # Generates the possible children of the Node
     def generate_possible_moves(self, max_step):
         move_directions = ((-1, 0, 0), (0, 1, 1), (1, 0, 2), (0, -1, 3)) # Move Up, Right, Down, Left 
         
+        # Make a difference betweeen agent's turn and opponent's turn
         if self.agent_turn:
             current_pos = self.my_pos
             other_pos = self.adv_pos
-            new_node_fn = lambda new_chess_board, pos, dir: Node(self, new_chess_board, pos, dir, self.adv_pos, not self.agent_turn)
+            new_node_fn = lambda new_chess_board, pos, dir: Node(self, new_chess_board, pos, dir, self.adv_pos, not self.agent_turn) 
         else:
             current_pos = self.adv_pos
             other_pos = self.my_pos
-            new_node_fn = lambda new_chess_board, pos, dir: Node(self, new_chess_board, self.my_pos, dir, pos, not self.agent_turn)
+            new_node_fn = lambda new_chess_board, pos, dir: Node(self, new_chess_board, self.my_pos, dir, pos, not self.agent_turn) # Note we swap the position with adv
         
+        # Initialize a queue to keep track of the nodes we are adding to possible children 
         queue = [(current_pos, 0)]
+        # A list to keep track of the nodes we visited so far to avoid coming back on our steps 
         visited = []
 
+        # Perform Breadth-First Search to find all possible moves
         while queue:
             pos, step = queue.pop(0)
             
@@ -151,7 +164,8 @@ class Node:
 
             if step >= max_step:
                 continue
-
+            
+            # Find all the possible possitions we can move to
             for row, col, dir in move_directions:
                 if self.chess_board[pos[0], pos[1], dir]:
                     continue
@@ -161,6 +175,7 @@ class Node:
 
                 queue.append(((pos[0] + row, pos[1] + col), step + 1))
         
+        # For all possible positions, count how many walls we can place
         for pos in visited:
             for _, _ , dir in move_directions:
                 if self.chess_board[pos[0], pos[1], dir]:
@@ -168,16 +183,22 @@ class Node:
                 new_chess_board = deepcopy(self.chess_board)
                 new_chess_board[pos[0], pos[1], dir] = 1
                 new_node = new_node_fn(new_chess_board, pos, dir)
+                # Add a possible child here
                 self.possible_children.append((new_node, new_node.heuristic()))
 
+        # Sort the possible children according to a heuristic
+        # Sorting in ascending order for adversary because they seek to minimize our utility
+        # Sorting in descending order for agent because they want to maximize utility 
         self.possible_children = sorted(self.possible_children, key = lambda val: val[1], reverse=self.agent_turn)
         
+    # Selects the next children to expand according to our guess/heuristic of what a logical next move is 
     def push_next_children(self, n):
         temp = self.possible_children[:n]
         self.children += temp
         self.possible_children = self.possible_children[n:]
         return temp
         
+    # Generates the next possible children from a node and pushes them as children
     def expand(self, n, max_step):
         if self.expanded:
             return self.push_next_children(n)
@@ -186,7 +207,6 @@ class Node:
             self.expanded = True
             return self.push_next_children(n)
 
-     
     def backpropagate(self, wins, visits):
         node = self
         
@@ -210,7 +230,9 @@ class HeuristicRollout:
         return num_rollouts*ROLLOUT_DECAY
     
     # TODO misuse of num_rollouts + acc
+    # Recursive helper method to perform a rollout
     def rec_rollout(self, state_to_explore, num_rollouts, max_step):
+        # state_to_explore is a node
         state_to_explore.expand(0, max_step)
         
         acc = 0
@@ -242,19 +264,20 @@ class StudentAgent(Agent):
         super(StudentAgent, self).__init__()
         self.name = "StudentAgent"
         self.autoplay = True
-        self.dir_map = {0: 'u', 1: 'r', 2: 'd', 3: 'l'}
         self.root_state = None
         self.first_run = True # Initial state of the game
-        #self.current_state = self.root_state # Progress of the game
-        Node.heuristic = heuristic
+        Node.heuristic = heuristic # Redefine heuristic here
 
     def update_tree(self, chess_board, my_pos, adv_pos):
+        # 1st run of the game
         if not self.root_state:
             self.root_state = Node(None, chess_board, my_pos, None, adv_pos, True)
             return
 
+        # We are in the middle of the game
         next_state = Node(None, chess_board, my_pos, None, adv_pos, True)
 
+        # Perform a search with depth 2 best move for us given that the adversary will pick the worst for for us (minimax)
         def depth_2_search(node, depth):
             if depth == 2:
                 if node == next_state:
@@ -301,6 +324,7 @@ class StudentAgent(Agent):
             
             return uct_max, uct_max_child
 
+        # Leveraging the 30 seconds to expand our search
         if self.first_run:
             self.first_run = False
             z = FIRST_COMPUTATION_TIME
@@ -309,9 +333,9 @@ class StudentAgent(Agent):
         
         while time.time() - initial_time < z - TIME_DELTA:
             (uct, best_child) = find_best_uct(self.root_state)
-            expanded = best_child.expand(NODES_TO_EXPAND, max_step) # Expand children 
+            expanded = best_child.expand(NODES_TO_EXPAND, max_step) # Expand children (Search)
             for child in expanded:
-                child[0].rollout(NUM_ROLLOUTS, max_step) # Set number of rollouts
+                child[0].rollout(NUM_ROLLOUTS, max_step) # Set number of rollouts (apprx)
         
         best_child = None
         best_child_uct = -math.inf
@@ -328,7 +352,6 @@ class StudentAgent(Agent):
                     best_child_uct = min_child_uct
             else:
                 min_child_uct, min_child = min([(x.uct_value(n), x) for x, _ in children], key = lambda x: x[0])
-            #min(map(lambda child: (child.uct_value(n), child), direct_children.children + direct_children.possible_children), key=lambda pair: pair[0])
 
                 if min_child_uct > best_child_uct:
                     best_child = min_child.parent
